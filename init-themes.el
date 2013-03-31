d956e28c9fd6bead569c3d6dd91264c11c5c0278
Olaf Conradi <olaf@conradi.org>
2013-03-31 22:06:27 +0200

    Start simple

--- init-themes.el
+++ init-themes.el
@@ -3,15 +3,6 @@
 
 ;(when (>= emacs-major-version 24)
   (require-package theme-pkg) ;)
-
-(setq-default custom-enabled-themes '(birds-of-paradise-plus))
-
-(defun reapply-themes ()
-  "Forcibly load the themes listed in custom-enabled-themes."
-  (dolist (theme custom-enabled-themes)
-    (unless (custom-theme-p theme)
-      (load-theme theme t))))
-
-(add-hook 'after-init-hook 'reapply-themes)
+  (load-theme theme-name)
 
 (provide 'init-themes)
