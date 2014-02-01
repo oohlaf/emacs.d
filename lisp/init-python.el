fcfd361211a823a867be20d346a4dcc1b735d559
Olaf Conradi <olaf@conradi.org>
2014-02-02 00:45:31 +0100

    Python config improvements
     * Auto indent on return
     * Use flycheck when installed

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,5 +1,13 @@
 (when (>= emacs-major-version 24)
   (require-package 'elpy)
-  (elpy-enable))
+  (elpy-enable)
+  ;; Remove flymake mode from elpy when flycheck is installed
+  (when (package-installed-p 'flycheck)
+    (setq elpy-default-minor-modes
+          (remove 'flymake-mode
+                  elpy-default-minor-modes)))
+  ;; Auto indent
+  (define-key python-mode-map (kbd "RET")
+              'newline-and-indent))
 
 (provide 'init-python)
