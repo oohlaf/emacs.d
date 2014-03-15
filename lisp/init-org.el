e6e2b2d9f83932f1ce31977c2711cf3e9da6e1fe
Olaf Conradi <olaf@conradi.org>
2014-03-15 23:35:17 +0100

    Tweak org-mode

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,12 +1,22 @@
 (require-package 'org-fstree)
 
 (global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
+
+(setq org-agenda-files
+      (list "~/Documents/org/personal.org"
+	    "~/Documents/org/hp.org"
+	    "~/Documents/org/people.org"))
+
+(setq org-log-done t
+      org-completion-use-ido t
+      org-tag-column 80)
 
 ;; On Windows enable Outlook integration
 (if (or (eq system-type 'windows-nt)
         (eq system-type 'cygwin))
-  (require-package 'org-outlook))
+    (require-package 'org-outlook))
 
 (provide 'init-org)
