4bf9ce10cad640cce633b2035f7ba506f6cd631c
Olaf Conradi <olaf@conradi.org>
2014-03-15 23:34:42 +0100

    Remove emacs version check in init-python
    Emacs 24 became a prerequisite

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,13 +1,13 @@
-(when (>= emacs-major-version 24)
-  (require-package 'elpy)
-  (elpy-enable)
-  ;; Remove flymake mode from elpy when flycheck is installed
-  (when (package-installed-p 'flycheck)
-    (setq elpy-default-minor-modes
-          (remove 'flymake-mode
-                  elpy-default-minor-modes)))
-  ;; Auto indent
-  (define-key python-mode-map (kbd "RET")
-              'newline-and-indent))
+(require-package 'elpy)
+(elpy-enable)
+
+;; Remove flymake mode from elpy when flycheck is installed
+(when (package-installed-p 'flycheck)
+  (setq elpy-default-minor-modes
+	(remove 'flymake-mode
+		elpy-default-minor-modes)))
+
+;; Auto indent
+(define-key python-mode-map (kbd "RET") 'newline-and-indent)
 
 (provide 'init-python)
