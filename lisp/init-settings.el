e077a9936762dc70b8328fb79b3c17e8301c204e
Olaf Conradi <olaf@conradi.org>
2014-03-16 00:02:48 +0100

    More settings

--- lisp/init-settings.el
+++ lisp/init-settings.el
@@ -8,8 +8,16 @@
 ;; Sentences do not need double spaces to end. Period.
 (set-default 'sentence-end-double-space nil)
 
+;; Accept y or n to answer yes or no questions
+(defalias 'yes-or-no-p 'y-or-n-p)
+
 ;; Keep cursor away from edges
 (require-package 'smooth-scrolling)
+
+;; Key bindings
+(global-set-key (kbd "C-;") 'comment-or-uncomment-region)
+(global-set-key (kbd "C-+") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
 
 ;; Add parts of each file's directory to the buffer name if not unique
 (require 'uniquify)
