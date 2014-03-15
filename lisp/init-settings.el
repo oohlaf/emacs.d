761074d9e239c45a6f72173f6387a9d26e2f66a7
Olaf Conradi <olaf@conradi.org>
2014-03-15 23:33:49 +0100

    Re-indent init-settings

--- lisp/init-settings.el
+++ lisp/init-settings.el
@@ -1,17 +1,15 @@
-(setq
- ;; Always show line and column numbers
- line-number-mode t
- column-number-mode t
- ;; Use all columns (default is 72)
- fill-column 80
- ;; Avoid beeping by flashing the window
- visible-bell t)
-
-;; Keep cursor away from edges
-(require-package 'smooth-scrolling)
+(setq line-number-mode t
+      column-number-mode t
+      ;; Use all columns (default is 72)
+      fill-column 80
+      ;; Avoid beeping by flashing the window
+      visible-bell t)
 
 ;; Sentences do not need double spaces to end. Period.
 (set-default 'sentence-end-double-space nil)
+
+;; Keep cursor away from edges
+(require-package 'smooth-scrolling)
 
 ;; Add parts of each file's directory to the buffer name if not unique
 (require 'uniquify)
